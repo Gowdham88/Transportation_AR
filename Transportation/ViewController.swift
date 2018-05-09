bfa8918674e86e5e8682175a9d6d94775a5da67a
Gowdham <gowdhaman@czsm.co.in>
2018-05-09 17:10:23 +0530

    ..

--- Transportation/ViewController.swift
+++ Transportation/ViewController.swift
@@ -21,13 +21,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +32,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = .horizontal
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -50,6 +47,67 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
+    }
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//        if let touch = touches.first {
+//
+//            let touchloaction = touch.location(in: sceneView)
+//
+//            let result = sceneView.hitTest(touchloaction, types: .existingPlaneUsingExtent)
+//
+//            if let hitresult = result.first {
+//
+//                let boxScene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//                if let shipNode = boxScene.rootNode.childNode(withName: "roomPortal", recursively: true) {
+//
+//                    shipNode.position = SCNVector3(
+//                        x: hitresult.worldTransform.columns.3.x,
+//                        y: hitresult.worldTransform.columns.3.y,
+//                        z: hitresult.worldTransform.columns.3.z
+//                    )
+//
+//                    sceneView.scene.rootNode.addChildNode(shipNode)
+//                }
+//
+//            }
+//            
+//        }
+//
+//    }
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        
+        if anchor is ARPlaneAnchor {
+            
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y:0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.jpg")
+            
+            plane.materials = [gridMaterial]
+            
+            node.addChildNode(planeNode)
+            
+            
+            
+            
+        } else {
+            
+            return
+        }
+        
     }
 
     // MARK: - ARSCNViewDelegate
