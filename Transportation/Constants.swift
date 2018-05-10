979f38c67d19c121c4b268d606cd1dff4189f103
CZSM4 <poojithanalluri80@gmail.com>
2018-05-10 19:56:42 +0530

    create class for childnodes

---
+++ Transportation/Constants.swift
@@ -0,0 +1,85 @@
+//
+//  Constants.swift
+//  Transportation
+//
+//  Created by CZSM4 on 10/05/18.
+//  Copyright © 2018 CZSM2. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+struct Constants {
+    
+//    static var sharedInstance = Constants()
+    
+    static var Felge1      : SCNNode!
+    static var kreuzschra  : SCNNode!
+    static var grill1      : SCNNode!
+    static var lemkrad     : SCNNode!
+    static var carosse_a   : SCNNode!
+    static var grill_gitt  : SCNNode!
+    static var unterboden  : SCNNode!
+    static var chrome      : SCNNode!
+    static var ausenspieg  : SCNNode!
+    static var brems5      : SCNNode!
+    static var brems3       : SCNNode!
+    static var scheinwerf   : SCNNode!
+    static var stiff        : SCNNode!
+    static var brems9       : SCNNode!
+    static var Reife        : SCNNode!
+    static var brems8       : SCNNode!
+    static var kreuz1       : SCNNode!
+    static var stiffte      : SCNNode!
+    static var stoff        : SCNNode!
+    static var _lsch        : SCNNode!
+    static var brems        : SCNNode!
+    static var seitenfens   : SCNNode!
+    static var bremsschei   : SCNNode!
+    static var fenster_sc   : SCNNode!
+    static var brems6       : SCNNode!
+    static var brems4       : SCNNode!
+    static var grill5       : SCNNode!
+    static var brems1       : SCNNode!
+    static var bremsicht_   : SCNNode!
+    static var brems_satt   : SCNNode!
+    static var spoiler      : SCNNode!
+    static var bremslicht   : SCNNode!
+    static var grill6       : SCNNode!
+    static var brembo       : SCNNode!
+    static var frontlicht   : SCNNode!
+    static var Reifen       : SCNNode!
+    static var frontschei   : SCNNode!
+    static var fenstergumL  : SCNNode!
+    static var carbone_he   : SCNNode!
+    static var radkasten    : SCNNode!
+    static var chrome_fen   : SCNNode!
+    static var grill2       : SCNNode!
+    static var _lsch1       : SCNNode!
+    static var seitensche   : SCNNode!
+    static var bremb1       : SCNNode!
+    static var _lsch2       : SCNNode!
+    static var brems7       : SCNNode!
+    static var motorhaube   : SCNNode!
+    static var bremb        : SCNNode!
+    static var grill_schw   : SCNNode!
+    static var computer     : SCNNode!
+    static var bremsbacke   : SCNNode!
+    static var stiff1       : SCNNode!
+    static var brems2       : SCNNode!
+    static var Felge2       : SCNNode!
+    static var front_gril   : SCNNode!
+    static var _lsch4       : SCNNode!
+    static var plaste       : SCNNode!
+    static var Reife1       : SCNNode!
+    static var lenkrad_na   : SCNNode!
+    static var grill3       : SCNNode!
+    static var _lschlauch   : SCNNode!
+    static var kreuz        : SCNNode!
+    static var grill4       : SCNNode!
+    static var Felge        : SCNNode!
+    static var _lsch3       : SCNNode!
+    static var heckscheid   : SCNNode!
+}
+
